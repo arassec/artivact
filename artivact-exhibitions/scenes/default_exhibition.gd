e8c6e012833ba502f922e61ca1cd770bfe5ed90a
Andreas Sensen <andreas.sensen@arassec.com>
2023-12-31 11:49:55 +0100

    Added first version of "Artivact Exhibitions".

---
+++ artivact-exhibitions/scenes/default_exhibition.gd
@@ -0,0 +1,42 @@
+extends Node3D
+
+var exhibitionId: String
+var exhibitionZipFile: String
+var exhibitionData: Variant
+
+var zipReader: ZIPReader
+
+var currentTopic
+
+
+func setup(exhibitionIdInput: String):
+	exhibitionId = exhibitionIdInput
+	# TODO: Replace "res://" with "user://" once downloading exhibitions is implemented!
+	exhibitionZipFile = str("res://", exhibitionId, ".artivact-exhibition.zip")
+	
+	zipReader = ZIPReader.new()
+	var openResult := zipReader.open(exhibitionZipFile)
+	if openResult != OK:
+		# TODO: Error handling!
+		return
+	
+	var exhibitionJson := JSON.new()
+	var exhibitionJsonString = zipReader.read_file(str(exhibitionId, ".artivact.json")).get_string_from_utf8()
+	var parseResult := exhibitionJson.parse(exhibitionJsonString)
+	if parseResult != OK:
+		# TODO: Error handling!
+		return
+
+	exhibitionData = exhibitionJson.data
+
+
+func _enter_tree():
+	currentTopic = load("res://scenes/default_topic.tscn").instantiate()
+	# TODO: Implement switching topics!
+	currentTopic.setup(zipReader, exhibitionData.topics[0])
+	add_child(currentTopic)
+
+
+func _exit_tree():
+	zipReader.close()
+
---
+++ artivact-exhibitions/scenes/default_multi_tool.gd
@@ -0,0 +1,75 @@
+extends Node3D
+
+var zipReader:ZIPReader
+var toolData: Variant
+
+var title: String
+
+var items = []
+var itemIndex = 0
+var maxItemIndex = 0
+
+var loadItem = false
+var itemLoaded = false
+var itemShown = false
+var loadedItem
+var shownItem
+
+
+func setup(zipReaderInput: ZIPReader, toolDataInput: Variant):
+	zipReader = zipReaderInput
+	toolData = toolDataInput
+		
+	if toolData.type == "TITLE":
+		title = toolData.title.value
+	elif toolData.type == "ITEMS":
+		items = toolData.itemIds
+		maxItemIndex = items.size() - 1
+
+	SignalBus.register(SignalBus.SignalType.NEXT_ITEM, next_item)
+	
+
+func next_item():
+	SignalBus.trigger_with_payload(SignalBus.SignalType.DEBUG, "next_item()")
+	itemIndex = itemIndex + 1
+	if itemIndex > maxItemIndex:
+		itemIndex = 0
+	loadItem = true
+	
+
+func _enter_tree():
+	if items.size() > 0:
+		loadItem = true
+
+
+func _exit_tree():
+	SignalBus.deregister(SignalBus.SignalType.NEXT_ITEM, next_item)
+
+
+func _process(_delta: float):
+	if loadItem:
+		loadItem = false
+		_load_item()
+	elif itemShown && itemLoaded:
+		_remove_item()
+	elif itemLoaded:
+		itemLoaded = false
+		_add_item()
+
+
+func _load_item():
+	loadedItem = load("res://scenes/default_item.tscn").instantiate()
+	loadedItem.setup(zipReader, items[itemIndex])
+	itemLoaded = true
+
+
+func _remove_item():
+	shownItem.visible = false
+	shownItem.queue_free()
+	itemShown = false
+
+
+func _add_item():
+	add_child(loadedItem)
+	shownItem = loadedItem
+	itemShown = true
