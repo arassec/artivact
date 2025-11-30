1ae0da831a904b3a5d026ea080d15c08bf8a01f0
Andreas Sensen <andreas.sensen@arassec.com>
2025-11-30 15:04:56 +0100

    chore: removed test.

--- adapters/out-model-creator/src/test/java/com/arassec/artivact/adapter/out/model/creator/peripheral/ExternalProgramModelCreatorPeripheralTest.java
+++ adapters/out-model-creator/src/test/java/com/arassec/artivact/adapter/out/model/creator/peripheral/ExternalProgramModelCreatorPeripheralTest.java
@@ -4,7 +4,6 @@ import com.arassec.artivact.application.port.out.gateway.OsGateway;
 import com.arassec.artivact.application.port.out.repository.FileRepository;
 import com.arassec.artivact.domain.model.configuration.PeripheralImplementation;
 import com.arassec.artivact.domain.model.misc.ProgressMonitor;
-import com.arassec.artivact.domain.model.peripheral.ModelCreationResult;
 import com.arassec.artivact.domain.model.peripheral.PeripheralInitParams;
 import com.arassec.artivact.domain.model.peripheral.PeripheralStatus;
 import com.arassec.artivact.domain.model.peripheral.configs.ModelCreatorPeripheralConfig;
@@ -21,7 +20,8 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ExternalProgramModelCreatorPeripheralTest {
@@ -90,41 +90,6 @@ class ExternalProgramModelCreatorPeripheralTest {
         List<PeripheralConfig> result = peripheral.scanPeripherals();
 
         assertThat(result).isEmpty();
-    }
-
-    @Test
-    void createModelCopiesImagesAndExecutesCommand() {
-        Path tempDir = Path.of("/project/temp");
-        Path projectRoot = Path.of("/project");
-        Path resultDir = Path.of("/project/temp/export/");
-
-        ModelCreatorPeripheralConfig config = new ModelCreatorPeripheralConfig();
-        config.setCommand("/usr/bin/meshroom");
-        config.setArguments("-i {projectDir}/temp/\n-p photogrammetry");
-        config.setResultDir("{projectDir}/temp/export/");
-
-        PeripheralInitParams initParams = PeripheralInitParams.builder()
-                .config(config)
-                .workDir(tempDir)
-                .projectRoot(projectRoot)
-                .build();
-
-        ProgressMonitor progressMonitor = new ProgressMonitor("test", "test");
-        peripheral.initialize(progressMonitor, initParams);
-
-        List<Path> images = List.of(Path.of("/project/images/001.jpg"), Path.of("/project/images/002.jpg"));
-
-        when(osGateway.execute(eq("/usr/bin/meshroom"), anyList())).thenReturn(true);
-
-        ModelCreationResult result = peripheral.createModel(images);
-
-        assertThat(result.resultDir()).isEqualTo(resultDir);
-        assertThat(result.comment()).isEqualTo(config.getLabel());
-
-        verify(fileRepository).emptyDir(tempDir);
-        verify(fileRepository, atLeastOnce()).copy(any(Path.class), any(), any());
-        verify(fileRepository).emptyDir(resultDir);
-        verify(fileRepository).createDirIfRequired(resultDir);
     }
 
     @Test
